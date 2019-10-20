a45f0c1ef59a62ed82367277fbd60bd44d613041
Vlad Eliseev <freshymanchan@gmail.com>
2019-10-20 21:16:24 +1000

    Storage was refactored, note queries was written

--- src/commonMain/kotlin/renote_core/db.kt
+++ src/commonMain/kotlin/renote_core/db.kt
@@ -1,27 +1,70 @@
 package renote_core
 
 import com.squareup.sqldelight.db.SqlDriver
-import renoteDB.Categories
 import renoteDB.CategoriesQueries
+import renoteDB.NotesQueries
+import renoteDB.AssociatedCategoriesQueries
 import renoteDB.MainDatabase
 
 expect fun createSysSqlDriver(dbName:String): SqlDriver
 
 class Storage() {
     private val db: MainDatabase
-    private val categories: CategoriesQueries
+    val categoriesDAO: CategoriesQueries
+    val notesDAO: NotesQueries
+    val associatedCategoriesDAO: AssociatedCategoriesQueries
 
     init {
         db = createMainDB()
-        categories = db.categoriesQueries
+        categoriesDAO = db.categoriesQueries
+        notesDAO = db.notesQueries
+        associatedCategoriesDAO = db.associatedCategoriesQueries
     }
 
-    fun addCategory(name: String) {
-        categories.insertCategory(name)
+    fun addCategory(name: String): Category {
+        categoriesDAO.insertCategory(name)
+        val categoryId = categoriesDAO.last_insert_rowid().executeAsOne()
+        return Category(categoryId, name)
     }
 
-    fun getCategories(): List<Categories> {
-        return categories.selectAllCategories().executeAsList()
+    fun getCategories(): List<Category> {
+        return categoriesDAO.selectAllCategories(
+            mapper = { id, name -> Category(id, name) }
+        ).executeAsList()
+    }
+
+    fun getNotes(): List<Note> {
+        return notesDAO.selectAllNotes(
+            mapper = { id, name, text, attachedLink -> Note(id, name, text, attachedLink) }
+        ).executeAsList()
+    }
+
+    fun getNotesForCategory(id: Long): List<Note> {
+        return associatedCategoriesDAO.getCategoryNotes(
+            id,
+            mapper = {noteId, name, text, attachedLink, _, _ -> Note(noteId, name, text, attachedLink)}
+        ).executeAsList()
+    }
+
+    fun getNotesForCategories(categoriesIds: List<Long>): List<Note> {
+        var result = listOf<Note>()
+
+        for (id in categoriesIds) {
+            result = result.plus(getNotesForCategory(id))
+        }
+
+        return result.distinctBy { note -> note.id }
+    }
+
+    fun addNote(name: String, text: String, attachedLink: String?, attachedCategories: List<Long>): Note {
+        notesDAO.insertNote(name, text, attachedLink)
+        val noteId = notesDAO.last_insert_rowid().executeAsOne()
+
+        for (attachedCategoryId in attachedCategories) {
+            associatedCategoriesDAO.insertAssociatedCategory(noteId, attachedCategoryId)
+        }
+
+        return Note(noteId, name, text, attachedLink)
     }
 
     private fun createMainDB(): MainDatabase {
