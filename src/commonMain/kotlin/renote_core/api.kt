01959e850443a83ea71da049ad13605905c642f2
Vlad Eliseev <freshymanchan@gmail.com>
2019-10-20 21:13:06 +1000

    Note data class was written

--- src/commonMain/kotlin/renote_core/api.kt
+++ src/commonMain/kotlin/renote_core/api.kt
@@ -1,5 +1,17 @@
 package renote_core
 
+data class Category(
+    val id: Long,
+    val name: String
+)
+
+data class Note(
+    val id: Long,
+    val name: String,
+    val text: String,
+    val attachedLink: String?
+)
+
 class RenoteAPI() {
 
     private val storage = Storage()
